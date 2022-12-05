982e447c4884609da44f62be96d20b1801bdb228
nishalamichhane <nishalamichhane9@gmail.com>
2022-12-05 19:53:48 +0100

    Private Blog commited

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Pagina</h1>
+            <p>Welcome in fantastic blogplatform. Below, we’ll cover the best self-hosted blogging platforms out there in detail,
+                and why they are the best choices on the market. “Self-hosted” means that you need a separate server and
+                domain to use the blogging software to create your blog or website.</p>
+        </div>
+    );
+};
+export default Home;
